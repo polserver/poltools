622bc613b5eb1bc14a5540ff5d2a76d42574b17c
AustinHeilman <AustinHeilman@0f0f479d-2c14-0410-9819-d12ebb206e83>
2008-09-01 20:33:29 +0000

    Completed the UI for configuring paths. Onto the configuration storage class.

--- tools/polLaunch/POLLaunch/Config/ConfigurationForm.Designer.cs
+++ tools/polLaunch/POLLaunch/Config/ConfigurationForm.Designer.cs
@@ -31,11 +31,34 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigurationForm));
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
 			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button5 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button6 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button7 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button8 = new System.Windows.Forms.Button();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
 			this.tabControl1.SuspendLayout();
 			this.tabPage1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tabControl1
@@ -51,6 +74,7 @@
 			// tabPage1
 			// 
 			this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(218)))), ((int)(((byte)(229)))));
+			this.tabPage1.Controls.Add(this.panel1);
 			this.tabPage1.Controls.Add(this.button3);
 			this.tabPage1.Controls.Add(this.button2);
 			this.tabPage1.Controls.Add(this.button1);
@@ -61,14 +85,14 @@
 			this.tabPage1.TabIndex = 0;
 			this.tabPage1.Text = "Paths";
 			// 
-			// button1
+			// button3
 			// 
-			this.button1.Location = new System.Drawing.Point(276, 357);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "Apply";
-			this.button1.UseVisualStyleBackColor = true;
+			this.button3.Location = new System.Drawing.Point(195, 357);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Okay";
+			this.button3.UseVisualStyleBackColor = true;
 			// 
 			// button2
 			// 
@@ -80,14 +104,212 @@
 			this.button2.UseVisualStyleBackColor = true;
 			this.button2.Click += new System.EventHandler(this.button2_Click);
 			// 
-			// button3
+			// button1
 			// 
-			this.button3.Location = new System.Drawing.Point(195, 357);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(75, 23);
-			this.button3.TabIndex = 2;
-			this.button3.Text = "Okay";
-			this.button3.UseVisualStyleBackColor = true;
+			this.button1.Location = new System.Drawing.Point(276, 357);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Apply";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			this.panel1.AutoScroll = true;
+			this.panel1.Controls.Add(this.groupBox2);
+			this.panel1.Controls.Add(this.groupBox1);
+			this.panel1.Location = new System.Drawing.Point(3, 3);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(620, 348);
+			this.panel1.TabIndex = 3;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.button4);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Location = new System.Drawing.Point(4, 4);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(588, 75);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Ultima Online";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(180)))), ((int)(((byte)(209)))));
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Location = new System.Drawing.Point(7, 46);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(575, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(7, 17);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 1;
+			this.button4.Text = "Browse";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(88, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(192, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Path to where Ultima Online is installed.";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label7);
+			this.groupBox2.Controls.Add(this.label6);
+			this.groupBox2.Controls.Add(this.label5);
+			this.groupBox2.Controls.Add(this.button8);
+			this.groupBox2.Controls.Add(this.textBox5);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.button7);
+			this.groupBox2.Controls.Add(this.textBox4);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Controls.Add(this.button6);
+			this.groupBox2.Controls.Add(this.textBox3);
+			this.groupBox2.Controls.Add(this.label2);
+			this.groupBox2.Controls.Add(this.button5);
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Location = new System.Drawing.Point(4, 85);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(588, 260);
+			this.groupBox2.TabIndex = 3;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "POL";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(88, 22);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(139, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Path to POL\'s root directory.";
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(7, 17);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 1;
+			this.button5.Text = "Browse";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(180)))), ((int)(((byte)(209)))));
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox2.Location = new System.Drawing.Point(7, 46);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(575, 20);
+			this.textBox2.TabIndex = 0;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(87, 82);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(188, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Path to the POL executable (POL.exe)";
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(6, 77);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 23);
+			this.button6.TabIndex = 4;
+			this.button6.Text = "Browse";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// textBox3
+			// 
+			this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(180)))), ((int)(((byte)(209)))));
+			this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox3.Location = new System.Drawing.Point(6, 106);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(575, 20);
+			this.textBox3.TabIndex = 3;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(88, 142);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(252, 13);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Path to the UOConvert executable (UOConvert.exe)";
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(7, 137);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(75, 23);
+			this.button7.TabIndex = 7;
+			this.button7.Text = "Browse";
+			this.button7.UseVisualStyleBackColor = true;
+			// 
+			// textBox4
+			// 
+			this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(180)))), ((int)(((byte)(209)))));
+			this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox4.Location = new System.Drawing.Point(6, 166);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(575, 20);
+			this.textBox4.TabIndex = 6;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(87, 202);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(233, 13);
+			this.label5.TabIndex = 11;
+			this.label5.Text = "Path to the ECompile executable (Ecompile.exe)";
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(6, 197);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(75, 23);
+			this.button8.TabIndex = 10;
+			this.button8.Text = "Browse";
+			this.button8.UseVisualStyleBackColor = true;
+			// 
+			// textBox5
+			// 
+			this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(180)))), ((int)(((byte)(209)))));
+			this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox5.Location = new System.Drawing.Point(6, 226);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(575, 20);
+			this.textBox5.TabIndex = 9;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(403, 142);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(178, 13);
+			this.label6.TabIndex = 12;
+			this.label6.Text = "(Should be in the POL root directory)";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(372, 202);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(209, 13);
+			this.label7.TabIndex = 13;
+			this.label7.Text = "(Should be in the pol root /scripts directory)";
 			// 
 			// ConfigurationForm
 			// 
@@ -106,6 +328,11 @@
 			this.Load += new System.EventHandler(this.ConfigurationForm_Load);
 			this.tabControl1.ResumeLayout(false);
 			this.tabPage1.ResumeLayout(false);
+			this.panel1.ResumeLayout(false);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -117,6 +344,26 @@
 		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
 
 	}
 }
--- tools/polLaunch/POLLaunch/Config/ConfigurationForm.cs
+++ tools/polLaunch/POLLaunch/Config/ConfigurationForm.cs
@@ -22,7 +22,7 @@ namespace POLLaunch.Configuration
 
 		private void button2_Click(object sender, EventArgs e)
 		{
-
+			this.Close();
 		}
 	}
 }
