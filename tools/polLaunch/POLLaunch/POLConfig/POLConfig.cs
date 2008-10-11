cf287c7c46241682a204f2b5bddbad54f9cb5966
AustinHeilman <AustinHeilman@0f0f479d-2c14-0410-9819-d12ebb206e83>
2008-10-11 19:14:14 +0000

    Working on a POL config parser.

---
+++ tools/polLaunch/POLLaunch/POLConfig/POLConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Windows.Forms;
+
+
+namespace POLConfig
+{
+	class POLConfig
+	{
+		const uint FLAG_STRUCTUREDCFG = 0x1;
+		const uint FLAG_FLATCFG = 0x2;
+
+		private uint _flags = 0x0;
+		private string _path = "";		
+
+		public POLConfig(string path): this(path, FLAG_STRUCTUREDCFG)
+		{
+		}
+
+		public POLConfig(string path, uint flags)
+		{
+			_flags = flags;
+			_path = path;
+
+			ReadConfigFile();
+		}
+
+		public bool ReadConfigFile()
+		{
+			if (!File.Exists(_path))
+				return false;
+			return true;
+		}
+	}
+}
