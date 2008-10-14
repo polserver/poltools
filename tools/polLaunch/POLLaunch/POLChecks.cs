7badaf1ee6b4f66d539e2f895fd6d2990ee4e1f8
AustinHeilman <AustinHeilman@0f0f479d-2c14-0410-9819-d12ebb206e83>
2008-10-14 15:33:53 +0000

    Thanks Turley for pointing out this was missing from the svn.

---
+++ tools/polLaunch/POLLaunch/POLChecks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace POLLaunch
+{
+	static class POLChecks
+	{
+		static public void ScriptChecks(ref TextBox textbox)
+		{
+			string[] src = FileSystemUtil.GetAllFileNames((string)Settings.Global.Properties["POLPath"], "*.src");
+			string[] ecl = FileSystemUtil.GetAllFileNames((string)Settings.Global.Properties["POLPath"], "*.ecl");
+			if (ecl.Length < src.Length)
+				textbox.AppendText("* Warning: Not all scripts are compiled." + Environment.NewLine);
+			else if (ecl.Length > src.Length)
+				textbox.AppendText("* Warning: There are more ecl files than src files." + Environment.NewLine);
+			else
+				textbox.AppendText("* Pass: All scripts are compiled." + Environment.NewLine);
+			textbox.AppendText("Found " + src.Length.ToString() + " .src files and " + ecl.Length.ToString() + " .ecl files." + Environment.NewLine);
+		}
+
+		static public void RealmChecks(ref TextBox textbox)
+		{
+			if (!Directory.Exists((string)Settings.Global.Properties["POLPath"] + "realm"))
+				textbox.AppendText("* Fail: Realms have not been generated." + Environment.NewLine);
+			else
+			{
+				string[] tmp = FileSystemUtil.GetAllFileNames((string)Settings.Global.Properties["POLPath"] + "\\realm", "*.*");
+				if (tmp.Length > 1)
+					textbox.AppendText("* Pass: Realm folder detected." + Environment.NewLine);
+				else
+					textbox.AppendText("* Fail: Realm folder is empty." + Environment.NewLine);
+			}
+
+			foreach (string s in UOConvert.GetConfigFileNames())
+			{
+				if (File.Exists((string)Settings.Global.Properties["POLPath"] + s))
+					textbox.AppendText("* Pass: Found config file '" + s + "'." + Environment.NewLine);
+				else
+					textbox.AppendText("* Fail: Config file '" + s + "' not found." + Environment.NewLine);
+			}
+		}
+	}
+}
