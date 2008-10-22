4159a7fb8061ddba3baea3f5c1533db9e2270d80
Fernando Rozenblit <rozenblit@gmail.com>
2008-10-22 15:36:09 +0000

    POLLaunch now runs POL and accepts console commands (including Ctrl! But doesn´t raise Ctrl-C signal ...)

--- tools/polLaunch/POLLaunch/Form1.cs
+++ tools/polLaunch/POLLaunch/Form1.cs
@@ -5,11 +5,16 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using POLLaunch.Console;
+using System.IO;
 
 namespace POLLaunch
 {
-	public partial class Form1 : Form
+    public partial class Form1 : Form
 	{
+        MyConsole POLConsole = null;
+        bool Ctrl, Alt, Shift; // small kludge... will it work?
+
 		public Form1()
 		{
 			InitializeComponent();
@@ -56,5 +61,74 @@ namespace POLLaunch
 
 			Cursor = Cursors.Default;
 		}
+
+        private void BTN_StartPOL_Click(object sender, EventArgs e)
+        {
+            if (this.POLConsole != null)
+            { // already running
+                MessageBox.Show("POL's already running"); // How?!
+                return;
+            }
+
+            string exepath = Settings.Global.Properties["POLExePath"];
+            string dirpath = Settings.Global.Properties["POLPath"];
+
+            if (!File.Exists(exepath))
+            {
+                MessageBox.Show("File does not exist in POLExePath!");
+                return;
+            }
+
+            if (!Directory.Exists(dirpath))
+            {
+                dirpath = Path.GetDirectoryName(exepath);
+            }
+             
+            // begin pol console...          
+
+            this.POLConsole = new MyConsole();
+            this.POLConsole.Start(Path.GetFullPath(exepath), Path.GetFullPath(dirpath));
+            this.POLConsole.Exited += new EventHandler(POLConsole_Exited);
+            this.POLConsole.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler(POLConsole_OutputDataReceived);
+            BTN_StartPOL.Enabled = false;
+        }
+
+        void POLConsole_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            if (e.Data != String.Empty)
+                txtPOLConsole.Invoke((MethodInvoker)delegate() { txtPOLConsole.Text += e.Data + System.Environment.NewLine; });
+        }
+
+        void POLConsole_Exited(object sender, EventArgs e)
+        {
+            txtPOLConsole.Invoke((MethodInvoker)delegate() { txtPOLConsole.Text += "<Process exited>" + System.Environment.NewLine; });
+
+            ((MyConsole)sender).Dispose();
+            this.POLConsole = null;
+
+            BTN_StartPOL.Invoke((MethodInvoker)delegate() { BTN_StartPOL.Enabled = true; });
+        }
+
+        private void txtPOLConsole_KeyUp(object sender, KeyEventArgs e)
+        {
+            this.Ctrl = e.Control;
+            this.Alt = e.Alt;
+            this.Shift = e.Shift;
+        }
+        private void txtPOLConsole_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (this.POLConsole != null)
+            {
+                this.POLConsole.Write(e.KeyChar);
+            }
+            e.Handled = true;
+        }
+
+        private void txtPOLConsole_TextChanged(object sender, EventArgs e)
+        {
+            // Auto scrolling
+            txtPOLConsole.Select(txtPOLConsole.Text.Length, 0);
+            txtPOLConsole.ScrollToCaret();
+        }
 	}
 }
