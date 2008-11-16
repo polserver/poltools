edb0821dd5252217b3033917e9bf2b9bb9bb2fcd
Turley <Turley@0f0f479d-2c14-0410-9819-d12ebb206e83>
2008-11-16 18:58:44 +0000

    Version 1.8 -Map tab: Added Zoom context menu option Added Show Statics context menu option Added navigation via mouse movement plus left mousebutton

--- tools/UoViewer/Controls/Map.cs
+++ tools/UoViewer/Controls/Map.cs
@@ -19,6 +19,7 @@ using System.Windows.Forms;
 using Ultima;
 using System.IO;
 using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
 
 namespace Controls
 {
@@ -40,45 +41,53 @@ namespace Controls
         private static Brush brush = new SolidBrush(Color.FromArgb(180, Color.White));
         private Pen pen = new Pen(brush);
         private Point currPoint;
+        private double Zoom = 1;
+        bool moving = false;
+        Point movingpoint;
 
         private void OnLoad(object sender, EventArgs e)
         {
             currmap = Ultima.Map.Felucca;
             feluccaToolStripMenuItem.Checked = true;
-            map=currmap.GetImage(0, 0,pictureBox.Right >>3,pictureBox.Bottom>>3 ,ShowStatics);
-
+            map = currmap.GetImage(0, 0, (int)(pictureBox.Right / Zoom)+8 >> 3, (int)(pictureBox.Bottom / Zoom)+8 >> 3, ShowStatics);
+            ZoomMap(ref map);
             pictureBox.Image = map;
+            ZoomLabel.Text = String.Format("Zoom: {0}",Zoom);
             SetScrollBarValues();
             Refresh();
         }
 
         private void HandleScroll(object sender, ScrollEventArgs e)
         {
-            map = currmap.GetImage(hScrollBar.Value >> 3, vScrollBar.Value >> 3, pictureBox.Right >> 3, pictureBox.Bottom >> 3, ShowStatics);
+            map = currmap.GetImage(hScrollBar.Value >> 3, vScrollBar.Value >> 3, (int)(pictureBox.Right / Zoom)+8 >> 3, (int)(pictureBox.Bottom / Zoom)+8 >> 3, ShowStatics);
+            ZoomMap(ref map);
             pictureBox.Image = map;
             pictureBox.Update();
+        }
+        
+        private void ZoomMap(ref Bitmap bmp0)
+        {
+            Bitmap bmp1 = new Bitmap((int)(map.Width*Zoom),(int)(map.Height*Zoom));
+            Graphics graph = Graphics.FromImage(bmp1);
+            graph.InterpolationMode = InterpolationMode.NearestNeighbor;
+            graph.PixelOffsetMode = PixelOffsetMode.Half;
+            graph.DrawImage(bmp0,new Rectangle(0,0,bmp1.Width,bmp1.Height));
+            graph.Dispose();
+            bmp0 = bmp1;
         }
 
         public void SetScrollBarValues()
         {
-
             this.vScrollBar.Minimum = 0;
             this.hScrollBar.Minimum = 0;
-
-            this.hScrollBar.Maximum = currmap.Width;
-
+            this.hScrollBar.Maximum = (int)(currmap.Width / Zoom);
             this.hScrollBar.LargeChange = 40;
             this.hScrollBar.SmallChange = 8;
-
             this.hScrollBar.Maximum += this.hScrollBar.LargeChange;
-
-            this.vScrollBar.Maximum = currmap.Height;
-
+            this.vScrollBar.Maximum = (int)(currmap.Height / Zoom);
             this.vScrollBar.LargeChange = 40;
             this.vScrollBar.SmallChange = 8;
-
             this.vScrollBar.Maximum += this.vScrollBar.LargeChange;
-
             this.vScrollBar.Value = 0;
             this.hScrollBar.Value = 0;
         }
@@ -87,7 +96,8 @@ namespace Controls
         {
             if (pictureBox.Image != null)
             {
-                map = currmap.GetImage(hScrollBar.Value >> 3, vScrollBar.Value >> 3, pictureBox.ClientSize.Width >> 3, pictureBox.ClientSize.Height >> 3, ShowStatics);
+                map = currmap.GetImage(hScrollBar.Value >> 3, vScrollBar.Value >> 3, (int)(pictureBox.ClientSize.Width / Zoom)+8 >> 3, (int)(pictureBox.ClientSize.Height / Zoom)+8 >> 3, ShowStatics);
+                ZoomMap(ref map);
                 pictureBox.Image = map;
                 pictureBox.Update();
             }
@@ -95,7 +105,8 @@ namespace Controls
 
         private void ChangeMap()
         {
-            map = currmap.GetImage(0, 0, pictureBox.Right >> 3, pictureBox.Bottom >> 3, ShowStatics);
+            map = currmap.GetImage(0, 0, (int)(pictureBox.Right / Zoom)+8 >> 3, (int)(pictureBox.Bottom / Zoom)+8 >> 3, ShowStatics);
+            ZoomMap(ref map);
             pictureBox.Image = map;
             pictureBox.Update();
             SetScrollBarValues();
@@ -191,16 +202,39 @@ namespace Controls
                 return;
             
             string FileName = Path.Combine(path, name);
-            Bitmap extract = currmap.GetImage(0, 0, (currmap.Width >> 3), (currmap.Height >> 3));
+            Bitmap extract = currmap.GetImage(0, 0, (currmap.Width >> 3)+8, (currmap.Height >> 3)+8);
             extract.Save(FileName, ImageFormat.Tiff);
             
         }
 
         private void OnMouseMove(object sender, MouseEventArgs e)
         {
-            int xDelta = e.X + hScrollBar.Value;
-			int yDelta = e.Y + vScrollBar.Value;
+            int xDelta = (int)(e.X / Zoom) + hScrollBar.Value;
+            int yDelta = (int)(e.Y / Zoom) + vScrollBar.Value;
             CoordsLabel.Text=String.Format("Coords: {0},{1}",xDelta,yDelta);
+            if (moving)
+            {
+                int deltax = (int)(-1 * (e.X - movingpoint.X) / Zoom);
+                int deltay = (int)(-1 * (e.Y - movingpoint.Y) / Zoom);
+                movingpoint.X = e.X;
+                movingpoint.Y = e.Y;
+                if (hScrollBar.Value + deltax < 0)
+                    hScrollBar.Value = 0;
+                else if (hScrollBar.Value + deltax > hScrollBar.Maximum)
+                    hScrollBar.Value = hScrollBar.Maximum;
+                else
+                    hScrollBar.Value += deltax;
+                if (vScrollBar.Value + deltay < 0)
+                    vScrollBar.Value = 0;
+                else if (vScrollBar.Value + deltax > vScrollBar.Maximum)
+                    vScrollBar.Value = vScrollBar.Maximum;
+                else
+                    vScrollBar.Value += deltay;
+                map = currmap.GetImage(hScrollBar.Value >> 3, vScrollBar.Value >> 3, (int)(pictureBox.Right / Zoom)+8 >> 3, (int)(pictureBox.Bottom / Zoom)+8 >> 3, ShowStatics);
+                ZoomMap(ref map);
+                pictureBox.Image = map;
+                pictureBox.Update();
+            }
         }
 
         private void onClick_ShowClientLoc(object sender, EventArgs e)
@@ -241,16 +275,13 @@ namespace Controls
                 tokunoToolStripMenuItem.Checked = true;
                 currmap = Ultima.Map.Tokuno;
             }
-            int screenX = x-pictureBox.Right/2;
-            int screenY = y-pictureBox.Bottom/2;
-            if (screenX < 0)
-                screenX = 0;
-            if (screenY < 0)
-                screenY = 0;
+            int screenX = (int)Math.Max(0,x-pictureBox.Right/Zoom/2);
+            int screenY = (int)Math.Max(0,y-pictureBox.Bottom/Zoom/2);
             SetScrollBarValues();
             hScrollBar.Value = screenX;
             vScrollBar.Value = screenY;
-            map = currmap.GetImage(screenX>>3, screenY>>3, pictureBox.Right >> 3, pictureBox.Bottom >> 3, ShowStatics);
+            map = currmap.GetImage(screenX >> 3, screenY >> 3, (int)(pictureBox.Right / Zoom)+8 >> 3, (int)(pictureBox.Bottom / Zoom)+8 >> 3, ShowStatics);
+            ZoomMap(ref map);
             pictureBox.Image = map;
             pictureBox.Update();
             string mapname = "Britannia";
@@ -300,8 +331,62 @@ namespace Controls
         private void OnOpenContext(object sender, CancelEventArgs e)  // Speichern für GetMapInfo
         {
             currPoint = pictureBox.PointToClient(Control.MousePosition);
+            currPoint.X = (int)(currPoint.X / Zoom);
+            currPoint.Y = (int)(currPoint.Y / Zoom);
             currPoint.X += hScrollBar.Value;
             currPoint.Y += vScrollBar.Value;
+        }
+
+        private void OnZoomMinus(object sender, EventArgs e)
+        {
+            Zoom /= 2;
+            DoZoom(true);
+        }
+
+        private void OnZoomPlus(object sender, EventArgs e)
+        {
+            Zoom *= 2;
+            DoZoom(false);
+        }
+
+        private void DoZoom(bool minus)
+        {
+            ZoomLabel.Text = String.Format("Zoom: {0}", Zoom);
+            int x, y;
+            x = Math.Max(0,currPoint.X - (int)(pictureBox.ClientSize.Width / Zoom) / 2);
+            y = Math.Max(0,currPoint.Y - (int)(pictureBox.ClientSize.Height / Zoom) / 2);
+            map = currmap.GetImage(x >> 3, y >> 3, (int)(pictureBox.ClientSize.Width / Zoom)+8 >> 3, (int)(pictureBox.ClientSize.Height / Zoom)+8 >> 3, ShowStatics);
+            ZoomMap(ref map);
+            pictureBox.Image = map;
+            pictureBox.Update();
+            hScrollBar.Value = Math.Min(hScrollBar.Maximum, x);
+            vScrollBar.Value = Math.Min(vScrollBar.Maximum, y);
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                moving = true;
+                movingpoint.X = e.X;
+                movingpoint.Y = e.Y;
+            }
+            else
+                moving = false;
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+        }
+
+        private void ClickShowStatics(object sender, EventArgs e)
+        {
+            ShowStatics = !ShowStatics;
+            map = currmap.GetImage(hScrollBar.Value >> 3, vScrollBar.Value >> 3, (int)(pictureBox.ClientSize.Width / Zoom) + 8 >> 3, (int)(pictureBox.ClientSize.Height / Zoom) + 8 >> 3, ShowStatics);
+            ZoomMap(ref map);
+            pictureBox.Image = map;
+            pictureBox.Update();
         }
     }
 }
