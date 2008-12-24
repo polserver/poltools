7ad53b3506ec19934a60299bf4efaa9107a829e6
muaddib_pol <muaddib_pol@0f0f479d-2c14-0410-9819-d12ebb206e83>
2008-12-24 04:25:11 +0000

    Initial POLUtils Project.

---
+++ tools/polUtils/POLUtils/AuxSvc.cs
@@ -0,0 +1,190 @@
+﻿/***************************************************************************
+ *
+ * $Author: MuadDib
+ *
+ * "THE BEER-WARE LICENSE"
+ * As long as you retain this notice you can do whatever you want with 
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ *
+ ***************************************************************************/
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace POLUtils.AuxSvc
+{
+    /// <summary>
+    ///     Provides a stream that implements the AuxSvc protocol.
+    /// </summary>
+    public class AuxSvcConnection
+    {
+
+        /// <summary>
+        ///     Privately holds the TCP socket for the AuxSvcConnection
+        /// </summary>
+        private TcpClient AuxSocket;
+
+        /// <summary>
+        ///     Privately holds the TCP Stream of the AuxSvcConnection
+        /// </summary>
+        private NetworkStream AuxStream;
+
+        /// <summary>
+        ///     Privately holds the StreamReader for the AuxSvcConnection
+        /// </summary>
+        private StreamReader AuxReader;
+
+        /// <summary>
+        ///     Privately holds the StreamWriter for the AuxSvcConnection
+        /// </summary>
+        private StreamWriter AuxWriter;
+
+        /// <summary>
+        ///     The default constructor is marked as private to
+        ///     ensure the parameterized constructor is called.
+        /// </summary>
+        private AuxSvcConnection()
+        {
+        }
+
+        /// <summary>
+        ///     Creates an AuxSvc object against the specified socket to designated Server IP and Server Port.
+        ///     It then initializes and connects the AuxSocket and uses the AuxSocket to initialize the AuxStream for our Read/Write work.
+        /// </summary>
+        /// <param name="server_ip">
+        ///     The ip that the <see cref="AuxSvcStream"/> will try to connect to.
+        /// </param>
+        /// <param name="port">
+        ///     The port that the <see cref="AuxSvcStream"/> will try to connect to.
+        /// </param>
+        public AuxSvcConnection(string server_ip, int server_port)
+        {
+            try
+            {
+                AuxSocket = new TcpClient(server_ip, server_port);
+            }
+            catch
+            {
+                // FIXME: We need to make an exception build here for checking in the 
+                // end-user's code. 
+                return;
+            }
+            try
+            {
+                if (AuxSocket.Connected)
+                {
+                    AuxStream = AuxSocket.GetStream();
+                    AuxReader = new StreamReader(AuxStream);
+                    AuxWriter = new StreamWriter(AuxStream);
+                    AuxWriter.AutoFlush = true;
+                }
+                else
+                    throw new IOException("Aux Svc Connection Is Not Active!");
+            }
+            catch
+            {
+                // FIXME:  We need to make an exception build here for checking in the 
+                // end-user's code. 
+                return;
+            }
+        }
+
+        /// <summary>
+        ///     Returns the current contents of the AuxStream in string format.
+        /// </summary>
+        public string Read()
+        {
+            try
+            {
+                if (AuxSocket.Connected)
+                    return AuxReader.ReadLine();
+                else
+                    throw new IOException("Aux Svc Connection Is Not Active For Read!");
+            }
+            catch (System.OutOfMemoryException ex)
+            {
+                // FIXME:  We need to make an exception build here for checking in the 
+                // end-user's code. 
+                return ("System.OutOfMemoryException: " + ex);
+            }
+            catch (System.IO.IOException ex)
+            {
+                // FIXME:  We need to make an exception build here for checking in the 
+                // end-user's code. 
+                return ("System.IO.IOException: " + ex);
+            }
+        }
+
+        /// <summary>
+        ///     Attempts to send a string using the AuxWriter Stream. Returns a bool for success.
+        /// </summary>
+        /// <param name="input">
+        ///     The POL Style Packed string to send to the POL Server.
+        /// </param>
+
+        public bool Write(string input)
+        {
+            try
+            {
+                if (AuxSocket.Connected)
+                {
+                    AuxWriter.WriteLine(input);
+                    return true;
+                }
+                else
+                    throw new IOException("Aux Svc Connection Is Not Active For Write!");
+            }
+            catch (System.ObjectDisposedException)
+            {
+                // FIXME:  We need to make an exception build here for checking in the 
+                // end-user's code. 
+                return false;
+            }
+            catch (System.NotSupportedException)
+            {
+                // FIXME:  We need to make an exception build here for checking in the 
+                // end-user's code. 
+                return false;
+            }
+            catch (System.IO.IOException)
+            {
+                // FIXME:  We need to make an exception build here for checking in the 
+                // end-user's code. 
+                return false;
+            }
+        }
+
+        /// <summary>
+        ///     Closes all AuxSvcConnection related Streams. Can be used to manually kill 
+        ///     an AuxSvcConnection if an error is encountered before using Write(). Will 
+        ///     result in the AuxSvc Script in POL to exit the While(connection) Loop and 
+        ///     terminate.
+        /// </summary>
+        public void Close()
+        {
+            AuxReader.Close();
+            AuxWriter.Close();
+            AuxStream.Close();
+            AuxSocket.Close();
+        }
+
+        /// <summary>
+        ///     Returns whether or not the AuxSvcConnection is connected to the remote host
+        ///     on the underlying socket.
+        /// </summary>
+        public bool Active
+        {
+            get
+            {
+                if (AuxSocket == null)
+                    return false;
+                else
+                    return this.AuxSocket.Connected;
+            }
+        }
+
+    }
+}
---
+++ tools/polUtils/POLUtils/PackUnpack.cs
@@ -0,0 +1,225 @@
+﻿/***************************************************************************
+ *
+ * $Author: Nando
+ *
+ * "THE BEER-WARE LICENSE"
+ * As long as you retain this notice you can do whatever you want with 
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ *
+ ***************************************************************************/
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Globalization;
+
+
+namespace POLUtils.PackUnpack
+{
+    public class PackUnpack
+    {
+
+        /// <summary>
+        ///     The default constructor is marked as private to
+        ///     ensure nothing funky. :)
+        /// </summary>
+        private PackUnpack()
+        {
+        }
+
+        /// <summary>
+        ///     Attempts to convert known Objects to a POL Style Packed Data String.
+        /// </summary>
+        /// <param name="ob">
+        ///     The Object to convert to POL Packed Data (Such as String, Int, Dictionary, etc)
+        /// </param>
+        public static string Pack(object ob)
+        {
+            return Pack(ob, false);
+        }
+
+        /// <summary>
+        ///     Attempts to convert known Objects to a POL Style Packed Data String.
+        /// </summary>
+        /// <param name="obj">
+        ///     The Object to convert to POL Packed Data (Such as String, Int, Dictionary, etc)
+        /// </param>
+        /// <param name="InArray">
+        ///     Tells the function if this Object was inside of an Array
+        /// </param>
+        private static string Pack(object obj, bool InArray)
+        {
+            if (obj == null) return "e0";
+
+            Type objtype = obj.GetType();
+
+            if (objtype.IsArray)
+            {
+                StringBuilder result = new StringBuilder();
+                result.AppendFormat("a{0}:", ((Array)obj).Length);
+                foreach (object elem in (Array)obj)
+                {
+                    result.Append(Pack(elem, true));
+                }
+                return result.ToString();
+            }
+
+            if (objtype == typeof(Hashtable)) // what are the others that should be here?
+            {
+                IDictionary dict = (IDictionary)obj;
+                StringBuilder result = new StringBuilder();
+                result.AppendFormat("d{0}:", dict.Keys.Count);
+
+                foreach (DictionaryEntry elem in dict)
+                {
+                    result.Append(Pack(elem.Key, true));
+                    result.Append(Pack(elem.Value, true));
+                }
+                return result.ToString();
+            }
+
+            if (objtype == typeof(System.String))
+            {
+                if (InArray)
+                    return String.Format("S{0}:{1}", ((string)obj).Length, (string)obj);
+                else
+                    return "s" + (string)obj;
+            }
+            else if (objtype == typeof(System.Int32))
+            {
+                return "i" + obj.ToString();
+            }
+            else if (objtype == typeof(System.Double))
+            {
+                return "r" + ((Double)obj).ToString(NumberFormatInfo.InvariantInfo);
+            }
+
+            return "u"; // If you can´t guess...
+        }
+
+        /// <summary>
+        ///     Attempts to convert a POL Packed Data String to the equivelant C# Object.
+        /// </summary>
+        /// <param name="packed">
+        ///     The POL Packed Data String to convert.
+        /// </param>
+        public static object Unpack(string packed)
+        {
+            switch (packed[0])
+            {
+                case 'd':
+                    throw new NotImplementedException();
+                case 't':
+                    throw new NotImplementedException();
+
+                // basic types
+                case 'a':
+                case 'i':
+                case 'r':
+                case 'S':
+                case 's':
+                    return UnpackHelper(packed);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        ///     Attempts to convert a POL Packed Data String to the equivelant C# Object.
+        /// </summary>
+        /// <param name="packed">
+        ///     The POL Packed Data String to convert.
+        /// </param>
+        private static object UnpackHelper(string packed)
+        {
+            switch (packed[0])
+            {
+                case 's':
+                    return packed.Remove(0, 1);
+                case 'i':
+                    return Convert.ToInt32(packed.Remove(0, 1));
+                case 'S':
+                    string arg = packed.Remove(0, 1);
+                    int pos = arg.IndexOf(':') + 1; // make it 1-based
+                    if (pos < 2) throw new ArgumentException("S needs a length");
+                    int len = Convert.ToInt32(arg.Substring(0, pos - 1));
+                    return arg.Substring(pos, Math.Min(len, arg.Length - pos));
+                case 'r':
+                    return Convert.ToDouble(packed.Substring(1), NumberFormatInfo.InvariantInfo);
+                case 'a':
+                    string PLine = packed.Remove(0, 1); // removes 'a'
+                    ArrayList result = new ArrayList();
+                    int ALPos = PLine.IndexOf(':');
+                    if (ALPos < 1) throw new ArgumentException("a needs a length");
+                    int PALength = Convert.ToInt32(PLine.Substring(0, ALPos));
+                    PLine = PLine.Remove(0, PLine.IndexOf(':')+1); // Remove a<length>:
+                    int counter;
+                    for (counter = 0; counter < PALength; counter++ )
+                    {
+                        // Get current element to unpack.
+                        // ElemLPos is invalid for 'r' and 'i'
+                        int ElemLPos = PLine.IndexOf(':');
+                        switch (PLine[0])
+                        {
+                            case 'a':
+                                throw new NotImplementedException();
+                            case 'd':
+                                throw new NotImplementedException();
+                            case 't':
+                                throw new NotImplementedException();
+
+                            // basic types
+                            case 'r':
+                            case 'i':
+                                int ElemEnd = 0;
+                                for (int j = 1; j < PLine.Length; j++ )
+                                {
+                                    switch (PLine[j])
+                                    {
+                                        case 'd':
+                                        case 't':
+                                        case 'a':
+                                        case 'i':
+                                        case 'r':
+                                        case 'S':
+                                        case 's':
+                                            // We found the next entry, stop here.
+                                            ElemEnd = j-1;
+                                            break;
+                                    }
+                                }
+                                if (ElemEnd < 1)
+                                {
+                                    // We are on the LAST element of the packed string
+                                    ElemEnd = PLine.Length-1; // Since last, use the Length.
+                                }
+                                if (PLine[0] == 'r')
+                                {
+                                    result.Add(Convert.ToDouble(PLine.Substring(1, ElemEnd)));
+                                }
+                                else
+                                {
+                                    result.Add(Convert.ToInt32(PLine.Substring(1, ElemEnd)));
+                                }
+                                PLine = PLine.Remove(0, (ElemEnd+1)); // remove the element
+                                break;
+                            case 'S':
+                                PLine = PLine.Remove(0, 1);
+                                int ElemPos = PLine.IndexOf(':') + 1; // make it 1-based
+                                if (ElemPos < 2) throw new ArgumentException("S needs a length");
+                                int ElemLen = Convert.ToInt32(PLine.Substring(0, ElemPos - 1));
+                                result.Add(PLine.Substring(ElemPos, Math.Min(ElemLen, PLine.Length - ElemPos)));
+                                PLine = PLine.Remove(0, (ElemLen + ElemPos));
+                                break;
+                        }
+
+                    }
+                    return result;
+                    
+                default: return null;
+            }
+        }
+    }
+}
