2b5b794f40ced789b9f5e30998b5d96865152601
muaddib_pol <muaddib_pol@0f0f479d-2c14-0410-9819-d12ebb206e83>
2008-12-24 04:34:43 +0000

    +PathStorage Class Object added.

---
+++ tools/polUtils/POLUtils/PathStorage.cs
@@ -0,0 +1,165 @@
+﻿/***************************************************************************
+ *
+ * $Author: MuadDib
+ *
+ * "THE BEER-WARE LICENSE"
+ * As long as you retain this notice you can do whatever you want with 
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ *
+ ***************************************************************************/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POLUtils.PathStorage
+{
+    public class PathStorage
+    {
+        /// <summary>
+        ///     Dictionary to contain all the Pathname storage.
+        /// </summary>
+        /// <key>Path social name string</key>
+        /// <value>Absolute Path String</value>
+        private static Dictionary<string, string> PathSettings = new Dictionary<string, string>();
+
+        /// <summary>
+        ///     Default Constructor
+        /// </summary>
+        public PathStorage() { InitPathSettings(); }
+
+        private static void InitPathSettings()
+        {
+            // I am lazy right now, sue me.
+            PathSettings.Add("UOPath", null);
+            PathSettings.Add("POLPath", null);
+            PathSettings.Add("POLExePath", null);
+            PathSettings.Add("UOConvertExePath", null);
+            PathSettings.Add("ECompileExePath", null);
+            PathSettings.Add("RunECLExePath", null);
+            PathSettings.Add("POLConfigPath", null);
+            PathSettings.Add("POLDataPath", null);
+            PathSettings.Add("POLLogPath", null);
+            PathSettings.Add("POLRealmPath", null);
+        }
+
+        /// <summary>
+        ///     Get all Path Social Names From The Path Storage.
+        /// </summary>
+        /// <return>A String Array of all the Key Values for the Path Storage.</return>
+        public string[] GetPathNames()
+        {
+            string[] PathNames = new string[PathSettings.Count];
+            int position = 0;
+            foreach (KeyValuePair<string, string> KVP in PathSettings)
+            {
+                PathNames.SetValue(KVP.Key.ToString(), position);
+                position++;
+            }
+            return PathNames;
+        }
+
+        /// <summary>
+        ///     Get the Path stored for a specific PathName.
+        /// </summary>
+        /// <param name="string PathName">PathName to look up the Path for.</param>
+        public string GetPath(string PathName)
+        {
+            if (PathSettings.ContainsKey(PathName))
+            {
+                return PathSettings[PathName].ToString();
+            }
+            else
+                throw new NotSupportedException("PathName supplied to GetPath() is not contained in Path Settings.");
+        }
+
+        /// <summary>
+        ///     Sets the Path stored for a specific PathName.
+        /// </summary>
+        /// <param name="string PathName">PathName to set the Path for.</param>
+        /// <param name="string NewValue">New string value to set the Path for.</param>
+        public bool SetPath(string PathName, string NewValue)
+        {
+            if (PathSettings.ContainsKey(PathName))
+            {
+                PathSettings[PathName] = NewValue;
+                return true;
+            }
+            else
+                throw new NotSupportedException("PathName supplied to SetPath() is not contained in Path Settings.");
+        }
+
+        /// <summary>
+        ///     Bool for if a PathName contains any data.
+        /// </summary>
+        /// <param name="string PathName">PathName to check.</param>
+        public bool IsSet(string PathName)
+        {
+            if (PathSettings.ContainsKey(PathName))
+            {
+                if (PathSettings[PathName] != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+                throw new NotSupportedException("PathName supplied to IsSet() is not contained in Path Settings.");
+
+        }
+
+        /// <summary>
+        ///     Adds a new PathName to the Path Settings.
+        /// </summary>
+        /// <param name="string PathName">PathName to add.</param>
+        /// <param name="string Value">Value to add to the PathName.</param>
+        public bool AddPath(string PathName, string Value)
+        {
+            if (!PathSettings.ContainsKey(PathName))
+            {
+                try
+                {
+                    PathSettings.Add(PathName, Value);
+                    return true;
+                }
+                catch (ArgumentException)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        ///     Remove a PathName from the Path Settings.
+        /// </summary>
+        /// <param name="string PathName">PathName to remove.</param>
+        public bool RemovePath(string PathName)
+        {
+            if (PathSettings.ContainsKey(PathName))
+            {
+                try
+                {
+                    PathSettings.Remove(PathName);
+                    return true;
+                }
+                catch (ArgumentNullException)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
