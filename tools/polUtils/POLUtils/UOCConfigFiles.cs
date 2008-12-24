306594b8bd27b8bd9fe717f5950b69fb19c73c99
muaddib_pol <muaddib_pol@0f0f479d-2c14-0410-9819-d12ebb206e83>
2008-12-24 04:52:48 +0000

    Added UOConvert base classes and objects.

---
+++ tools/polUtils/POLUtils/UOCConfigFiles.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+using POLUtils.UOConvert;
+using POLUtils.UOConvert.UOCConfigFiles;
+
+namespace POLUtils.UOConvert.UOCConfigFiles
+{
+    public class ConfigFiles
+    {
+        protected string _Filename = null;
+        protected string _UOCCommand = null;
+
+        public ConfigFiles() { }
+
+        public string GetFilename()
+        {
+            return _Filename;
+        }
+        public string GetUOCCommand()
+        {
+            return _UOCCommand;
+        }
+/*        public void BuildConfig(string UOConvertPath, string ConfigPath, bool UseUOPath)
+        {
+            string FileName = ConfigPath + "\\" + this.GetFilename();
+            string OldFileName = UOConvertPath + "\\" + this.GetFilename();
+            UOConvert.RunConverter(this.GetUOCCommand() + " <uopath>", UseUOPath);
+
+            try
+            {
+                File.Delete(FileName);
+            }
+            catch (Exception)
+            {
+                throw new Exception("BuildConfig() could not delete " + FileName);
+            }
+
+            try
+            {
+                File.Move(OldFileName, FileName);
+            }
+            catch (Exception)
+            {
+                throw new Exception("BuildConfig() could not move " + OldFileName + " to " + FileName);
+            }
+
+        } */
+    }
+
+    public class Multis : ConfigFiles
+    {
+        public Multis()
+        {
+            _Filename = "multis.cfg";
+            _UOCCommand = "multis";
+        }
+    }
+
+    public class Landtiles : ConfigFiles
+    {
+        public Landtiles()
+        {
+            _Filename = "landtiles.cfg";
+            _UOCCommand = "landtiles";
+        }
+    }
+
+    public class Tiledata : ConfigFiles
+    {
+        public Tiledata()
+        {
+            _Filename = "tiles.cfg";
+            _UOCCommand = "tiles";
+        }
+    }
+}
---
+++ tools/polUtils/POLUtils/UOConvert.cs
@@ -0,0 +1,39 @@
+﻿/***************************************************************************
+ *
+ * $Author: MuadDib
+ *
+ * "THE BEER-WARE LICENSE"
+ * As long as you retain this notice you can do whatever you want with 
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ *
+ ***************************************************************************/
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+using POLUtils.UOConvert.UOCRealms;
+using POLUtils.UOConvert.UOCConfigFiles;
+
+namespace POLUtils.UOConvert
+{
+    public class UOConvert
+    {
+        /// <summary>
+        ///     Dictionary to contain all the UOConvert Generated \Config\*.cfg storage.
+        /// </summary>
+        public ConfigFiles ConfigFiles = new ConfigFiles();
+
+        /// <summary>
+        ///     Dictionary to contain all the Realms storage.
+        /// </summary>
+        public Realms Realms = new Realms();
+
+        /// <summary>
+        ///     Default Constructor
+        /// </summary>
+        public UOConvert() { }
+
+    }
+}
