d9baaed3b088271fa47704e035cf279512b551ad
muaddib_pol <muaddib_pol@0f0f479d-2c14-0410-9819-d12ebb206e83>
2009-01-02 20:07:50 +0000

    1-2-2009 - MuadDib     Added  : False return for no key found in Flag() for ecompile class.

--- tools/polUtils/POLUtils/Console.cs
+++ tools/polUtils/POLUtils/Console.cs
@@ -1,9 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Runtime.InteropServices;
+﻿/***************************************************************************
+ *
+ * $Author: Nando
+ * $Edited: MuadDib
+ *
+ * "THE BEER-WARE LICENSE"
+ * As long as you retain this notice you can do whatever you want with 
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ *
+ ***************************************************************************/
+using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace POLLaunch.Console
 {
