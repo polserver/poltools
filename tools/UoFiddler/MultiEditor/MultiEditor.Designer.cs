23e0bf01c574811048ea7507f8bda7e9551c3ba5
muaddib_pol <muaddib_pol@0f0f479d-2c14-0410-9819-d12ebb206e83>
2009-05-04 12:44:35 +0000

    + Basic design for tabs in Multi Editor toolbox. Blah

--- tools/UoFiddler/MultiEditor/MultiEditor.Designer.cs
+++ tools/UoFiddler/MultiEditor/MultiEditor.Designer.cs
@@ -39,18 +39,73 @@ namespace FiddlerPlugin
         /// </summary>
         private void InitializeComponent()
         {
+            this.TC_MultiEditorToolbox = new System.Windows.Forms.TabControl();
+            this.tileTab = new System.Windows.Forms.TabPage();
+            this.designTab = new System.Windows.Forms.TabPage();
+            this.importTab = new System.Windows.Forms.TabPage();
+            this.TC_MultiEditorToolbox.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // TC_MultiEditorToolbox
+            // 
+            this.TC_MultiEditorToolbox.Controls.Add(this.tileTab);
+            this.TC_MultiEditorToolbox.Controls.Add(this.designTab);
+            this.TC_MultiEditorToolbox.Controls.Add(this.importTab);
+            this.TC_MultiEditorToolbox.Location = new System.Drawing.Point(4, 4);
+            this.TC_MultiEditorToolbox.Name = "TC_MultiEditorToolbox";
+            this.TC_MultiEditorToolbox.SelectedIndex = 0;
+            this.TC_MultiEditorToolbox.Size = new System.Drawing.Size(171, 317);
+            this.TC_MultiEditorToolbox.TabIndex = 0;
+            // 
+            // tileTab
+            // 
+            this.tileTab.BackColor = System.Drawing.SystemColors.Window;
+            this.tileTab.Location = new System.Drawing.Point(4, 22);
+            this.tileTab.Name = "tileTab";
+            this.tileTab.Padding = new System.Windows.Forms.Padding(3);
+            this.tileTab.Size = new System.Drawing.Size(163, 291);
+            this.tileTab.TabIndex = 0;
+            this.tileTab.Text = "Tiles";
+            this.tileTab.UseVisualStyleBackColor = true;
+            // 
+            // designTab
+            // 
+            this.designTab.BackColor = System.Drawing.SystemColors.Window;
+            this.designTab.Location = new System.Drawing.Point(4, 22);
+            this.designTab.Name = "designTab";
+            this.designTab.Padding = new System.Windows.Forms.Padding(3);
+            this.designTab.Size = new System.Drawing.Size(163, 291);
+            this.designTab.TabIndex = 1;
+            this.designTab.Text = "Design";
+            this.designTab.UseVisualStyleBackColor = true;
+            // 
+            // importTab
+            // 
+            this.importTab.BackColor = System.Drawing.SystemColors.Window;
+            this.importTab.Location = new System.Drawing.Point(4, 22);
+            this.importTab.Name = "importTab";
+            this.importTab.Size = new System.Drawing.Size(163, 291);
+            this.importTab.TabIndex = 2;
+            this.importTab.Text = "Import";
+            this.importTab.UseVisualStyleBackColor = true;
             // 
             // MultiEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TC_MultiEditorToolbox);
             this.Name = "MultiEditor";
             this.Size = new System.Drawing.Size(619, 324);
+            this.TC_MultiEditorToolbox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl TC_MultiEditorToolbox;
+        private System.Windows.Forms.TabPage tileTab;
+        private System.Windows.Forms.TabPage designTab;
+        private System.Windows.Forms.TabPage importTab;
     }
 }
