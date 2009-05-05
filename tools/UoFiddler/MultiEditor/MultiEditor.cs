238ee91cec542bc069985bf72716bcf1d029fbee
muaddib_pol <muaddib_pol@0f0f479d-2c14-0410-9819-d12ebb206e83>
2009-05-05 01:50:33 +0000

    + Some more design on Multi Editor. + multieditor.xml for toolbox. Same basis as from HouseBuilder, but reformatted cuz I prefer better layouts for options files. BOOOOOO. + Reading of XML for the tree view implemented. Next will be reading of the tiles from items tab for the XML. Blah.

--- tools/UoFiddler/MultiEditor/MultiEditor.cs
+++ tools/UoFiddler/MultiEditor/MultiEditor.cs
@@ -10,20 +10,78 @@
  ***************************************************************************/
 
 using System;
+using System.IO;
+using System.Xml;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
 using System.Text;
 using System.Windows.Forms;
+using System.Resources;
+using System.Reflection;
 
 namespace FiddlerPlugin
 {
     public partial class MultiEditor : UserControl
     {
+
+        private static MultiEditor refMarkerMulti = null;
+
         public MultiEditor()
         {
             InitializeComponent();
+            refMarkerMulti = this;
+            InitializeToolBox();
         }
+
+        private void InitializeToolBox()
+        {
+            string path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            string FileName = Path.Combine(path, @"plugins/multieditor.xml");
+            if (!File.Exists(FileName))
+                return;
+
+            XmlDocument dom = new XmlDocument();
+            dom.Load(FileName);
+            XmlElement xTiles = dom["TileGroups"];
+
+            foreach (XmlElement xRootGroup in xTiles)
+            {
+                TreeNode mainNode = new TreeNode();
+                mainNode.Text = xRootGroup.GetAttribute("name");
+                mainNode.Tag = null;
+
+                mainNode.ImageIndex = 0;
+
+                AddChildren(mainNode, xRootGroup);
+
+                treeViewTilesXML.Nodes.Add(mainNode);
+            }
+        }
+
+        private void AddChildren(TreeNode node, XmlElement mainNode)
+        {
+            foreach (XmlElement e in mainNode)
+            {
+                if (e.Name == "subgroup")
+                {
+                    TreeNode tempNode = new TreeNode();
+
+                    tempNode.Text = e.GetAttribute("name");
+                    tempNode.Tag = e.GetAttribute("index");
+                    tempNode.ImageIndex = 0;
+
+                    if (e.HasChildNodes)
+                    {
+                        AddChildren(tempNode, e);
+                    }
+
+                    node.Nodes.Add(tempNode);
+                }
+            }
+        }
+
     }
 }
