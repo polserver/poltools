5c59a3c75705dad08d250a7a271c7b2f1b282945
AustinHeilman <AustinHeilman@0f0f479d-2c14-0410-9819-d12ebb206e83>
2011-09-14 16:34:34 +0000

    Developing a tool to setup stuff in the new crafting system.

---
+++ tools/CraftTool/CraftTool/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CraftTool
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Forms.AboutForm about_form = new Forms.AboutForm();
+			about_form.ShowDialog(this);
+		}
+	}
+}
---
+++ tools/CraftTool/CraftTool/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace CraftTool
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static int Main()
+		{
+			Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+
+			return 0;
+		}
+
+
+		/// <summary>
+		/// Gets the folder path to where the EXE was run from (does not include the exe name)
+		/// </summary>
+		/// <param name="trailing_slash"></param>
+		/// <returns></returns>
+		static public string GetPath(bool trailing_slash)
+		{
+			string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+			path = path.Substring(0, path.LastIndexOf(@"\") + (trailing_slash == true ? 1 : 0));
+			return path;
+		}
+
+		private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+		{
+			Exception ex = e.Exception;
+			ExceptionBox.ExceptionForm tmp = new ExceptionBox.ExceptionForm("Unhandled Exception", "An unhandled thread exception has occured.", ref ex);
+			tmp.ShowDialog();
+		}
+
+		private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs args)
+		{
+			Exception ex = (Exception)args.ExceptionObject;
+			ExceptionBox.ExceptionForm tmp = new ExceptionBox.ExceptionForm("Unhandled Exception", "An unhandled exception has occured.", ref ex);
+			tmp.ShowDialog();
+		}
+
+		#region Assembly Attribute Accessors
+
+		static public string AssemblyTitle
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+				if (attributes.Length > 0)
+				{
+					AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+					if (titleAttribute.Title != "")
+					{
+						string title = titleAttribute.Title;
+						return title;
+					}
+				}
+				return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+			}
+		}
+
+		static public string AssemblyVersion
+		{
+			get
+			{
+				return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+			}
+		}
+
+		static public string AssemblyDescription
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+			}
+		}
+
+		static public string AssemblyProduct
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyProductAttribute)attributes[0]).Product;
+			}
+		}
+
+		static public string AssemblyCopyright
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+			}
+		}
+
+		static public string AssemblyCompany
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyCompanyAttribute)attributes[0]).Company;
+			}
+		}
+		#endregion
+	}
+}
